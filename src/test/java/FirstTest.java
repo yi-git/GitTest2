744ffcbafef5d1679fc2fa5b87f9112b251950d0
BB <51766381+yi-git@users.noreply.github.com>
2019-07-31 16:48:32 -0500

    Berdy (#5)
    * added my comment
    
    * question
    
    * added code
    
    * added code

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -5,10 +5,15 @@ public class FirstTest {
     }
 
         //made changes
-
         //added changes later "this is added by erdem"
+        //looks like evething works as expected - berdy
         //looks like evething works as expected - berdy
 
     // what do we need to test now?
+
+
+    // adding new code here
+    // long code
+
 
 }
