22c1f068e89f0fe00b18c588bcc352334c0be1be
BB <51766381+yi-git@users.noreply.github.com>
2019-07-31 15:23:17 -0500

    Berdy (#3)
    * added my comment
    
    * question

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,5 +2,7 @@ public class FirstTest {
 
         //made changes
         //added changes later "this is added by erdem"
+        //looks like evething works as expected - berdy
 
+    // what do we need to test now?
 }
