a98839fabeb201f1198a2383657a68c1646e90bf
Esra Akbulut <37938004+benesranur@users.noreply.github.com>
2019-07-31 17:29:32 -0400

    Esra (#4)
    * sending to master
    
    * Revert "second file added"
    
    This reverts commit 4db5b8d3ad7089500b216d29d0ac39cb1c1073cd.
    
    # Conflicts:
    #	src/main/java/Secondfile.java
    #	src/test/java/FirstTest.java
    
    * second file added
    
    (cherry picked from commit 4db5b8d3ad7089500b216d29d0ac39cb1c1073cd)
    
    * esra made these

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,14 @@
 public class FirstTest {
 
+    public static void main(String[] args) {
+        System.out.println("esra made these changes");
+    }
+
         //made changes
+
         //added changes later "this is added by erdem"
         //looks like evething works as expected - berdy
 
     // what do we need to test now?
+
 }
