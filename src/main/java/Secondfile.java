8b6ef9632ce90256438fd8f696b1da15a948701d
benesranur <ivirziviremailler@gmail.com>
2019-07-31 15:52:28 -0400

    sending to master

--- src/main/java/Secondfile.java
+++ src/main/java/Secondfile.java
@@ -2,5 +2,6 @@ public class Secondfile {
 
     public static void main(String[] args) {
         System.out.println("I am sending this to master");
+        System.out.println("now this one not in master");
     }
 }
